d81a0f1de221123b2ee93a8cb81f169899a064ca
ksuklik <ksuklik@gmail.com>
2021-10-29 22:31:01 +0300

    дз8.1

---
+++ js/modal.js
@@ -0,0 +1,18 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open]'),
+      closeModalBtn: document.querySelector('[data-modal-close]'),
+      modal: document.querySelector('[data-modal]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+
+
+
+      const a = {a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,}
+      refs.modal.classList.toggle('is-hidden');
+    }
+  })();
