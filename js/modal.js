213ba99a200ad56deebef6f7bf434c650b0c5dbb
ksuklik <ksuklik@gmail.com>
2021-10-30 16:43:10 +0300

    дз8.2

--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,14 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-modal-open]'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
-    };
-  
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
 
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-
-      const a = {a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,a:b,}
-      refs.modal.classList.toggle('is-hidden');
-    }
-  })();
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
